07ed99639844022f74d068a74671e894f35092c6
Chinmay R Mhatre <78205628+chinmay0910@users.noreply.github.com>
2026-03-08 23:27:23 +0530

    Update generate-readme-stats.js

--- scripts/generate-readme-stats.js
+++ scripts/generate-readme-stats.js
@@ -9,24 +9,6 @@ async function fetchData() {
   return { user, repos };
 }
 
-function generateStatsSVG(user, repos) {
-  const stars = repos.reduce((a, r) => a + r.stargazers_count, 0);
-  const forks = repos.reduce((a, r) => a + r.forks_count, 0);
-
-  return `
-<svg width="450" height="200" xmlns="http://www.w3.org/2000/svg">
-<rect width="100%" height="100%" fill="#0d1117"/>
-<text x="20" y="40" fill="white" font-size="18">GitHub Stats</text>
-
-<text x="20" y="80" fill="white">Public Repos: ${user.public_repos}</text>
-<text x="20" y="110" fill="white">Followers: ${user.followers}</text>
-<text x="20" y="140" fill="white">Stars: ${stars}</text>
-<text x="20" y="170" fill="white">Forks: ${forks}</text>
-
-</svg>
-`;
-}
-
 function generateLanguagesSVG(repos) {
   const langs = {}
 
@@ -90,12 +72,10 @@ function updateTopRepos(readme, repos) {
 }
 
 async function main() {
-  const { user, repos } = await fetchData();
+  const { repos } = await fetchData();
 
-  const statsSVG = generateStatsSVG(user, repos);
   const langSVG = generateLanguagesSVG(repos);
 
-  fs.writeFileSync("assets/github-stats.svg", statsSVG);
   fs.writeFileSync("assets/top-languages.svg", langSVG);
 
   const readme = fs.readFileSync("README.md","utf8");
@@ -103,7 +83,7 @@ async function main() {
 
   fs.writeFileSync("README.md", updated);
 
-  console.log("README stats updated");
+  console.log("README updated");
 }
 
 main();
